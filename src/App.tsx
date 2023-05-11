cb78b2c70808d601fd0a850156c2345a99cac1d8
mmesser-rr <michaelmesser0795@gmail.com>
2023-05-11 12:28:15 +0800

    impr: remove unnecessary components

--- src/App.tsx
+++ src/App.tsx
@@ -2,55 +2,15 @@ import { useState } from "react";
 // @ts-ignore
 import { WalletChatWidget } from 'react-wallet-chat-v0';
 import 'react-wallet-chat-v0/dist/index.css';
-import {
-  Connector,
-  useAccount,
-  useConnect,
-  useDisconnect,
-  useNetwork,
-  useSwitchNetwork
-} from 'wagmi';
-import { polygonMumbai } from 'wagmi/chains';
 
 function App() {
-  const { address, isConnected } = useAccount();
-  const { connect, connectors } = useConnect();
-  const { disconnect } = useDisconnect();
-  const { chain } = useNetwork();
-  const { switchNetwork } = useSwitchNetwork({
-    chainId: polygonMumbai.id,
-    throwForSwitchChainNotSupported: true,
-    onError(error) {
-      console.log('Switch Network Error:', error);
-    },
-  });
-
   const [widgetState, setWidgetState] = useState({});
-
-  const handleConnectWallet = (connector: Connector<any, any, any>) => {
-    if (chain?.unsupported) {
-      switchNetwork?.();
-    }
-    else if (isConnected) {
-      disconnect();
-    }
-    else {
-      connect({ connector });
-    }
-  };
 
   return (
     <>
       <div>
-        <h3>Manual Connect Wallet</h3>
-        {connectors.filter((connector) => connector.ready).map((connector) => (
-          <button key={connector.id} onClick={() => handleConnectWallet(connector)}>
-            {chain?.unsupported ? "Switch Network" : isConnected ? address : connector.name}
-          </button>
-        ))}
-      </div>
-
-      <div>
+        <h2>{`My name is Michael Messer! (discord handle: axecun#7885)`}</h2>
+        <br />
         <h3>Chat With Owner</h3>
         <p>Only click this button after connect wallet inside wallet-chat. Thank you!</p>
         <button
