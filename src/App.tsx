4fc6084015d59a872f6cb4d4e8a7ba05075207bc
mmesser-rr <michaelmesser0795@gmail.com>
2023-05-11 12:14:20 +0800

    feat: add chat with owner feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 // @ts-ignore
 import { WalletChatWidget } from 'react-wallet-chat-v0';
 import 'react-wallet-chat-v0/dist/index.css';
@@ -24,6 +25,8 @@ function App() {
     },
   });
 
+  const [widgetState, setWidgetState] = useState({});
+
   const handleConnectWallet = (connector: Connector<any, any, any>) => {
     if (chain?.unsupported) {
       switchNetwork?.();
@@ -38,16 +41,30 @@ function App() {
 
   return (
     <>
-      {connectors.filter((connector) => connector.ready).map((connector) => (
-        <button key={connector.id} onClick={() => handleConnectWallet(connector)}>
-          {chain?.unsupported ? "Switch Network" : isConnected ? address : connector.name}
-        </button>
-      ))}
+      <div>
+        <h3>Manual Connect Wallet</h3>
+        {connectors.filter((connector) => connector.ready).map((connector) => (
+          <button key={connector.id} onClick={() => handleConnectWallet(connector)}>
+            {chain?.unsupported ? "Switch Network" : isConnected ? address : connector.name}
+          </button>
+        ))}
+      </div>
 
-      <WalletChatWidget widgetState={{
-        chatAddr: "0x17FA0A61bf1719D12C08c61F211A063a58267A19",
-        isOpen: true
-      }} />
+      <div>
+        <h3>Chat With Owner</h3>
+        <p>Only click this button after connect wallet inside wallet-chat. Thank you!</p>
+        <button
+          onClick={() => setWidgetState({
+            chatAddr: "0x17FA0A61bf1719D12C08c61F211A063a58267A19",
+            isOpen: true
+          })}
+        >
+          Chat with Owner
+        </button>
+      </div>
+
+
+      <WalletChatWidget widgetState={widgetState} />
     </>
   );
 }
