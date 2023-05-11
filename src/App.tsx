8d2cf486a87da8caca81286fa521a1854870ff31
mmesser-rr <michaelmesser0795@gmail.com>
2023-05-11 12:05:48 +0800

    integrate wallet-chat

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// @ts-ignore
+import { WalletChatWidget } from 'react-wallet-chat-v0';
+import 'react-wallet-chat-v0/dist/index.css';
+import {
+  Connector,
+  useAccount,
+  useConnect,
+  useDisconnect,
+  useNetwork,
+  useSwitchNetwork
+} from 'wagmi';
+import { polygonMumbai } from 'wagmi/chains';
 
 function App() {
+  const { address, isConnected } = useAccount();
+  const { connect, connectors } = useConnect();
+  const { disconnect } = useDisconnect();
+  const { chain } = useNetwork();
+  const { switchNetwork } = useSwitchNetwork({
+    chainId: polygonMumbai.id,
+    throwForSwitchChainNotSupported: true,
+    onError(error) {
+      console.log('Switch Network Error:', error);
+    },
+  });
+
+  const handleConnectWallet = (connector: Connector<any, any, any>) => {
+    if (chain?.unsupported) {
+      switchNetwork?.();
+    }
+    else if (isConnected) {
+      disconnect();
+    }
+    else {
+      connect({ connector });
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {connectors.filter((connector) => connector.ready).map((connector) => (
+        <button key={connector.id} onClick={() => handleConnectWallet(connector)}>
+          {chain?.unsupported ? "Switch Network" : isConnected ? address : connector.name}
+        </button>
+      ))}
+
+      <WalletChatWidget widgetState={{
+        chatAddr: "0x17FA0A61bf1719D12C08c61F211A063a58267A19",
+        isOpen: true
+      }} />
+    </>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,50 @@
+// import { CoinbaseWalletConnector } from "@wagmi/core/connectors/coinbaseWallet";
+// import { MetaMaskConnector } from "@wagmi/core/connectors/metaMask";
+// import { WalletConnectConnector } from "@wagmi/core/connectors/walletConnect";
+import { CoinbaseWalletConnector } from "wagmi/connectors/coinbaseWallet";
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
+import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
+import { w3mProvider } from "@web3modal/ethereum";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { WagmiConfig, configureChains, createClient } from "wagmi";
+import { polygonMumbai } from "wagmi/chains";
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
+
+const chains = [polygonMumbai];
+const projectId = '9658be696c85857e496562d681b5d9d9';
+
+const { provider } = configureChains(chains, [w3mProvider({ projectId })]);
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors: [
+    new MetaMaskConnector({ chains }),
+    new WalletConnectConnector({
+      chains,
+      options: {
+        projectId: projectId,
+      },
+    }),
+    new CoinbaseWalletConnector({
+      chains,
+      options: {
+        appName: "wagmi",
+      },
+    }),
+  ],
+  provider,
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <WagmiConfig client={wagmiClient}>
+      <App />
+    </WagmiConfig>
   </React.StrictMode>
 );
 
